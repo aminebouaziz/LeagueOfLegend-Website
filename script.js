9b304b744b79323cfdeb71eeb7162b58250f463d
aminebouaziz <bouaziz.amine.18@gmail.com>
2019-12-17 11:46:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function kata() {
+  document.getElementById("kata").style.display = "block";
+  document.getElementById("zed").style.display = "none";
+  document.getElementById("yasuo").style.display = "none";
+}
+
+function zed() {
+  document.getElementById("kata").style.display = "none";
+  document.getElementById("zed").style.display = "block";
+  document.getElementById("yasuo").style.display = "none";
+}
+
+function yasuo() {
+  document.getElementById("kata").style.display = "none";
+  document.getElementById("zed").style.display = "none";
+  document.getElementById("yasuo").style.display = "block";
+}
